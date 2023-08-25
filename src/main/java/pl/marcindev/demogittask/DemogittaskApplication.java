58bf2001c0c12512f3583c0869cbe067c11e3552
Marcin <gasiorek.mail@gmail.com>
2023-08-25 13:22:32 +0200

    Initial commit

---
+++ src/main/java/pl/marcindev/demogittask/DemogittaskApplication.java
@@ -0,0 +1,13 @@
+package pl.marcindev.demogittask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogittaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogittaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcindev/demogittask/DemogittaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcindev.demogittask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogittaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
